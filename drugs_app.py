8053dbf6811ea69b5d36dd2a4c1ed965d66c2fe9
@franasal <farcilas@gmail.com>
2021-12-03 15:48:42 +0100

    :art: update theme

--- drugs_app.py
+++ drugs_app.py
@@ -5,12 +5,27 @@ import json
 import altair as alt
 from pathlib import Path
 
-# st.title(':bar_chart: COMPARE DRUG HARMS :mag:')
-st.markdown("## **[David Nutt's dangerous drug list](https://www.theguardian.com/science/2009/nov/02/david-nutt-dangerous-drug-list)**")
 
+st.set_page_config(
+     page_title="Compare Drug Harms",
+     page_icon="https://pbs.twimg.com/profile_images/1396102254487384065/ZjD8GvMw_400x400.png",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'Get Help': 'https://www.extremelycoolapp.com/help',
+         'Report a bug': "https://www.extremelycoolapp.com/bug",
+         'About': "# This is a header. This is an *extremely* cool app!"
+     }
+ )
 
-intro_markdown = Path("README.md").read_text()
+header = '# [<img src="https://pbs.twimg.com/profile_images/1396102254487384065/ZjD8GvMw_400x400.png" alt="drawing" width="50"/>ViewsOnDrugs<img src="https://pbs.twimg.com/media/E1_0586WQAYCNym?format=png&name=small" alt="drawing" width="50"/>](https://twitter.com/ViewsOnDrugsBot/)'
+
+st.markdown(header, unsafe_allow_html=True)
+# st.title("**[David Nutt's dangerous drug list](https://www.theguardian.com/science/2009/nov/02/david-nutt-dangerous-drug-list)**")
+intro_markdown = Path("info.md").read_text()
 st.markdown(intro_markdown, unsafe_allow_html=True)
+
+st.write("##")
 
 
 with open('./data/drug_categories.json', 'r') as json_file:
@@ -53,13 +68,14 @@ def main():
     end_drug_list = drug_list
     end_categories_list = categories_list
 
-    chart_placeholder = st.empty()
-    descr_placeholder = st.empty()
-    st.markdown("###  ① ** Let's pick some Drugs: **")
+
+    st.markdown("###  ① ** Pick some Drugs: **")
+    st.markdown("###  :pill: :tea: :mushroom: :herb: :lollipop: :diamond_shape_with_a_dot_inside: :smoking: :horse_racing::syringe: :coffee: :wine_glass: :candy:")
+
     substances = st.multiselect("", drug_list, []
     )
 
-    st.markdown("### ② **Let's choose some categories:**")
+    st.markdown("### ② **Choose some harm categories:**")
     sel_categories = st.multiselect("",categories.keys(), []
     )
 
@@ -70,7 +86,8 @@ def main():
 
         descripts= "\n\n".join([f"**{cat}** \n {categories[cat][0]}" for cat in sel_categories])
 
-        descr_placeholder.markdown(descripts)
+        descr_placeholder = st.empty()
+        descr_placeholder.info(descripts)
 
 
 
@@ -79,14 +96,13 @@ def main():
         y=alt.Y('sum(value)', title=None),
         color=alt.Color('Category', scale=alt.Scale(scheme='dark2')
          )
-        ).configure_legend(orient='right')
+        ).configure_legend(orient='bottom', columns=5, labelFontSize=10)
 
 
     # st.altair_chart(fig, use_container_width=True)
-
+    chart_placeholder = st.empty()
     chart_placeholder.altair_chart(fig, use_container_width=True)
 
-    st.markdown("## :diamond_shape_with_a_dot_inside: :smoking: :pill: :syringe: :wine_glass: :candy:")
 
 
 if __name__ == "__main__":
