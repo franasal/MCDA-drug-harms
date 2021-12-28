634093fa0111753dcc1444e03377e753f3111b9f
@franasal <farcilas@gmail.com>
2021-12-28 11:47:21 +0100

    reestructure data for collab readability

--- en_app.py
+++ en_app.py
@@ -7,33 +7,27 @@ from pathlib import Path
 
 lang="EN"
 
+
 def load_data(lang):
     path_ = "./data/lang"
 
-    labels_df = pd.read_csv(os.path.join(path_, "label_names.tsv"), sep = '\t')
-
-    indexes = labels_df[f'{lang}_VARIABLE'].dropna().tolist()
-    col_names = labels_df[f'{lang}_DRUG'].dropna().tolist()
-    lab_names = labels_df[f'{lang}_LABELS'].dropna().tolist()
-
-    data = pd.read_csv(os.path.join(path_, "input_table.tsv"), header=None, sep = '\t').rename(index=str, columns=labels_df[f'{lang}_DRUG'])
+    lang_df = pd.read_csv(os.path.join(path_, f"{lang}.tsv"), sep = '\t')
+    descriptions = lang_df[['VARIABLE','VARNAME','DESCRIPTION']].dropna().set_index('VARIABLE').T.to_dict("series")
+    lab_names = lang_df[[f'__loc_dont_modify_', 'LABEL']].dropna().set_index('__loc_dont_modify_').to_dict()['LABEL']
+    indexes=descriptions.keys()
+    col_names = lang_df['DRUG'].dropna().tolist()
+    data = pd.read_csv(os.path.join("./data/", "input_table.tsv"), header=None, sep = '\t', decimal=',').rename(columns=lang_df['DRUG'])
 
     data.index = list(indexes)
-    data.reset_index(inplace=True)
-    descriptions = pd.read_csv(os.path.join(path_, "descriptions.tsv"), sep = '\t')[[f'{lang}_VARIABLE',f'{lang}_VARNAME',f'{lang}_DESCRIPTION']].set_index(f'{lang}_VARIABLE').T.to_dict("series")
+    data = data.reset_index().T
+    data.columns = data.iloc[0]
+    data.columns.name = "Category"
+    data.drop('index', inplace=True)
 
-
-    return data.drop(16).T, descriptions, lab_names
+    return data, descriptions, lab_names
 
 
 transposed_df, categories, lab_names=load_data(lang)
-
-transposed_df.columns = transposed_df.iloc[0]
-transposed_df.columns.name = "Category"
-transposed_df.drop('index', inplace=True)
-transposed_df.drop('weight', inplace=True)
-#create stacked bar chart
-
 
 
 def create_plot(sel_substances, sel_categories):
@@ -44,7 +38,7 @@ def create_plot(sel_substances, sel_categories):
 
 def main():
     drug_list = sorted(transposed_df.index.tolist())
-    categories_list = sorted(transposed_df.columns.tolist()[1:])
+    categories_list = sorted(transposed_df.columns.tolist())
     intro_markdown = Path(f"{lang.lower()}_info.md").read_text()
     st.markdown(intro_markdown, unsafe_allow_html=True)
 
@@ -53,13 +47,14 @@ def main():
     col1, col2 = st.columns(2)
 
     with col1:
-        st.markdown(f"###  ① ** {lab_names[0]}: **")
-        st.text(f"{lab_names[1]}")
+        st.markdown(f"###  ① ** {lab_names['__select_d_title']}: **")
+        st.text(f"{lab_names['__select_d_subt']}")
         substances = st.multiselect("", drug_list, [])
         st.markdown("#####  :pill: :candy: :broccoli: :tea: :mushroom: :snowflake: :smoking: :horse_racing::syringe: :wine_glass:")
 
     with col2:
-        st.markdown(f"### ② **{lab_names[2]}:**")
+        st.markdown(f"### ② **{lab_names['__select_c_title']}:**")
+        st.text(f"{lab_names['__select_c_subt']}")
         sel_categories = st.multiselect("",categories_list, [])
 
     if substances and len(substances)>1:
@@ -67,7 +62,7 @@ def main():
     if sel_categories:
         categories_list = [x for x in sel_categories]
 
-        descripts = "\n\n".join([f"**{cat}** \n {categories[cat][f'{lang}_DESCRIPTION']}" for cat in sel_categories])
+        descripts = "\n\n".join([f"**{cat}** \n {categories[cat]['DESCRIPTION']}" for cat in sel_categories])
 
         descr_placeholder = st.empty()
         descr_placeholder.info(descripts)
@@ -87,7 +82,7 @@ def main():
     chart_placeholder.altair_chart(fig, use_container_width=True)
 
 
-    foot = f'#### {lab_names[3]}  [<img src="https://pbs.twimg.com/media/FGE5sFPX0AY6TtV?format=png&name=small"  alt="drawing" width="50"/>](https://mybrainmychoice.de/) [<img src="https://pbs.twimg.com/media/FGGjxH-XIAc101E?format=jpg&name=small" alt="drawing" width="50"/>](https://youthrise.org/) & [<img src="https://pbs.twimg.com/profile_images/1396102254487384065/ZjD8GvMw_400x400.png" alt="drawing" width="50"/> ViewsOnDrugs](https://twitter.com/ViewsOnDrugsBot/)'
+    foot = f'#### {lab_names["__foot"]}  [<img src="https://pbs.twimg.com/media/FGE5sFPX0AY6TtV?format=png&name=small"  alt="drawing" width="50"/>](https://mybrainmychoice.de/) [<img src="https://pbs.twimg.com/media/FGGjxH-XIAc101E?format=jpg&name=small" alt="drawing" width="50"/>](https://youthrise.org/) & [<img src="https://pbs.twimg.com/profile_images/1396102254487384065/ZjD8GvMw_400x400.png" alt="drawing" width="50"/> ViewsOnDrugs](https://twitter.com/ViewsOnDrugsBot/)'
     st.markdown(foot, unsafe_allow_html=True)
 
 if __name__ == "__main__":
